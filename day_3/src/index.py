d765ff9ae94fc70355657986c52035d723b98a3e
Antti Moisio <moisioa1@gmail.com>
2025-12-03 13:07:40 +0200

    Cleaned up

--- day_3/src/index.py
+++ day_3/src/index.py
@@ -1,15 +1,4 @@
-from itertools import combinations
-
-def get_highest_number(s, n):
-    res = 0
-    for digits in combinations(s, n):
-        total = 0
-        for i in range(n):
-            total = 10*total+int(digits[i])
-        res = max(res, total)
-    return res
-
-def find_highest_greedy(s, n, curr):
+def find_highest_number(s, n, curr):
     if not n:
         return curr
     l = len(s)
@@ -17,13 +6,13 @@ def find_highest_greedy(s, n, curr):
     d = max(prefix)
     i = s.find(d)
     curr = 10*curr+int(d)
-    return find_highest_greedy(s[i+1:], n-1, curr)
+    return find_highest_number(s[i+1:], n-1, curr)
 
 def part1(fname):
     res = 0
     with open(fname) as file:
         for line in file:
-            x = get_highest_number(line.strip(), 2)
+            x = find_highest_number(line.strip(), 2, 0)
             res+=x
             
     return res
@@ -32,7 +21,7 @@ def part2(fname):
     res = 0
     with open(fname) as file:
         for line in file:
-            x = find_highest_greedy(line.strip(), 12, 0)
+            x = find_highest_number(line.strip(), 12, 0)
             res+=x
     return res
 
