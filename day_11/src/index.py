5a84ffb95924e1d87856a5977518c9d2ce8b75e0
Antti Moisio <moisioa1@gmail.com>
2025-12-11 09:54:37 +0200

    Day 11 done

---
+++ day_11/src/index.py
@@ -0,0 +1,73 @@
+def read_input(fname):
+    graph = {}
+    with open(fname) as file:
+        for line in file:
+            line = line.strip()
+            parent, children = line.split(": ")
+            graph[parent] = children.split()
+    return graph
+
+def invert_graph(graph):
+    inverted = {}
+    for node, children in graph.items():
+        if node not in inverted:
+            inverted[node] = []
+        for c in children:
+            if c not in inverted:
+                inverted[c] = []
+            inverted[c].append(node)
+    return inverted
+
+def part1(graph):
+    ways = {"out": 1}
+    blacklist = set()
+    count_paths_from(graph, "you", ways, blacklist)
+    return ways["you"]
+
+def part2(graph):
+    #count ways from srv to fft that don't visit dac or out
+    ways = {"fft": 1}
+    blacklist = set(["out", "dac"])
+    count_paths_from(graph, "svr", ways, blacklist)
+    svr_to_fft = ways["svr"]
+    #count ways from src to dac that don't visit fft or out
+    ways = {"dac": 1}
+    blacklist = set(["out", "fft"])
+    count_paths_from(graph, "svr", ways, blacklist)
+    svr_to_dac = ways["svr"]
+    #count ways from dac to fft that don't visit out
+    ways = {"fft": 1}
+    blacklist = set(["out"])
+    count_paths_from(graph, "dac", ways, blacklist)
+    dac_to_fft = ways["dac"]
+    #count ways from fft to dac that don't visit out
+    ways = {"dac": 1}
+    count_paths_from(graph, "fft", ways, blacklist)
+    fft_to_dac = ways["fft"]
+    #count ways from fft to out that don't visit dac
+    ways = {"out": 1}
+    blacklist = set(["dac"])
+    count_paths_from(graph, "fft", ways, blacklist)
+    fft_to_out = ways["fft"]
+    #count ways from dac to out that don't visit fft
+    ways = {"out": 1}
+    blacklist = set(["fft"])
+    count_paths_from(graph, "dac", ways, blacklist)
+    dac_to_out = ways["dac"]
+    res = svr_to_dac*dac_to_fft*fft_to_out+svr_to_fft*fft_to_dac*dac_to_out
+    return res
+
+def count_paths_from(graph, node, ways, blacklist):
+    if node in ways:
+        return ways[node]
+    ways[node] = 0
+    for next_node in graph[node]:
+        if next_node in blacklist:
+            continue
+        ways[node]+=count_paths_from(graph, next_node, ways, blacklist)
+    return ways[node]
+
+if __name__=="__main__":
+    fname = "input.txt"
+    graph = read_input(fname)
+    print(part2(graph))
